f7b0321fa850628403b218582b246c297249a502
miguelarnaud <62722371+miguelarnaud@users.noreply.github.com>
2020-04-02 23:08:32 +0100

    Update ShopPrices.java

--- Kingdoms/src/pt/ubatv/kingdoms/commands/shop/ShopPrices.java
+++ Kingdoms/src/pt/ubatv/kingdoms/commands/shop/ShopPrices.java
@@ -17,6 +17,42 @@ public class ShopPrices {
     public int chiseledStoneBricks = 3;
     public int mossyCbble = 3;
     public int sand = 5;
+    public int redSand = 5;
+    public int sandstorm = 10;
+    public int chiseldSandstorm = 10;
+    public int oakLog = 32;
+    public int spruceLog = 32;
+    public int birchLog = 32;
+    public int jungleLog = 32;
+    public int acaciaLog = 32;
+    public int darkOakLog = 32;
+    public int gravel = 6;
+    public int clay = 10;
+    public int bricks = 25;
+    public int oakPlank = 7;
+    public int sprucePlank = 7;
+    public int birchPlank = 7;
+    public int junglePlanks = 7; 
+    public int acaciaPlanks = 7;
+    public int darkOakPlanks = 7;
+    public int ice = 10;
+    public int packedIce= 12;
+    public int endStone = 8;
+    public int netherrack = 5;
+    public int netherBrick = 18;
+    public int soulSand = 32;
+    public int obsidian 500;
+    public int magmaBlock = 250;
+    public int glowstone = 50;
+    public int blockQuartz = 65;
+    public int chiselesQuartzBlock = 65;
+    public int quartzPillar = 65;
+    public int prismarine = 100;
+    public int prismarineBlock = 100;
+    public int darkPrismarine = 100;
+    public int seaLantern = 100;
+    //sao so 5 euros filho da puta : miguelarnaudr@gmail.com beijinhos
+        
         
     
 }
