2e93234583d2acb1b55958796bd799e7a71973c0
miguelarnaud <62722371+miguelarnaud@users.noreply.github.com>
2020-04-02 22:58:26 +0100

    Update ShopPrices.java

--- Kingdoms/src/pt/ubatv/kingdoms/commands/shop/ShopPrices.java
+++ Kingdoms/src/pt/ubatv/kingdoms/commands/shop/ShopPrices.java
@@ -6,5 +6,17 @@ public class ShopPrices {
     public int stone = 5;
     public int granite = 3; 
     public int grass = 5;
+    public int diorite = 3;
+    public int andesite = 3;
+    public int dirt = 5;
+    public int podzol = 5;
+    public int mycelium = 5;
+    public int stoneBricks = 3;
+    public int mossyStoneBrick = 3;
+    public int crackedStoneBricks = 3;
+    public int chiseledStoneBricks = 3;
+    public int mossyCbble = 3;
+    public int sand = 5;
+        
     
 }
