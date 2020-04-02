0e5f54dea8dbfb6c506be8a4fe5c7a9ed65cea72
miguelarnaud <62722371+miguelarnaud@users.noreply.github.com>
2020-04-02 23:32:31 +0100

    Update ShopPrices.java

--- Kingdoms/src/pt/ubatv/kingdoms/commands/shop/ShopPrices.java
+++ Kingdoms/src/pt/ubatv/kingdoms/commands/shop/ShopPrices.java
@@ -51,7 +51,97 @@ public class ShopPrices {
     public int prismarineBlock = 100;
     public int darkPrismarine = 100;
     public int seaLantern = 100;
+    
     //sao so 5 euros filho da puta : miguelarnaudr@gmail.com beijinhos
+    
+    public int rawBeef = 35;
+    public int rawPockchop = 35;
+    public int rawMutton = 30;
+    public int rawChicken = 25;
+    public int rawRabit = 22;
+    public int wheatSeeds = 250;
+    public int wheat = 5;
+    public int steak = 50;
+    public int cookedPork = 50;
+    public int cookedMutton = 50;
+    public int cookedChick = 45;
+    public int cookedRabit = 32;
+    public int bread = 40;
+    public int hayBale = 43;
+    public int oakSap = 125;
+    public int spruceSap = 500;
+    public int birchSap = 125;
+    public int jungleSap = 350;
+    public int acaciaSap = 150;
+    public int darkOakSap = 500;
+    public int apple = 55;
+    public int brownMush = 45;
+    public int redMush = 45;
+    public int melonSeed = 500;
+    public int melonSlice = 500;
+    public int beetrootSeed = 500;
+    public int beetroot = 500;
+    public int punpkinSeed = 500;
+    public int carvedPump = 500;
+    public int pumpkinPie = 40;
+    public int carrot = 500;
+    public int potato = 500;
+    public int cactus = 550;
+    public int sugarCane = 550;
+    public int sugar = 25;
+    public int chorusFlower = 750;
+    public int netherWart = 600;
+    
+    //SELLING ONLY ORES//
+    
+    public int emerald = 20;
+    public int emeraldBlock = emerald*9;
+    public int emeraldOre = emerald*4;
+    public int diamond = 100;
+    public int diamondBlock = diamond*9;
+    public int diamondOre = diamond*4;
+    public int gold = 30;
+    public int goldBlock = gold*9;
+    public int goldOre = 5;
+    public int iron = 15;
+    public int ironBlock = iron*9;
+    public int ironOre = 5;
+    public int quartz = 5;
+    public int quartzBlock = quartz*9;
+    public int quartzOre = 3;
+    public int lapis = 10;
+    public int lapisBlock = lapis*9;
+    public int lapisOre = 5;
+    public int redstone = 10;
+    public int redstoneBlock = redstone*9;
+    public int redstoneOre = 5;
+    public int coal = 3;
+    public int coalBlock = coal*9;
+    public int coalOre = 3;
+    public int charcoal = 1;
+    
+    //SELL ONLY ORES FINISH//
+    
+    //MISC//
+    
+    public int allDiscs = 350;
+    public int waterBucket = 50;
+    public int lavaBucket = 500;
+    public int ironHorseArmor = 2000;
+    public int goldHorseArmor = 5000;
+    public int diamondHorseArmor = 8500;
+    public int nametag = 1000;
+    
+    //MISC finish//
+    
+    
+    
+    
+    
+    
+    
+    
+   
         
         
     
