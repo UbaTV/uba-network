6f66be7c6c7eccdd8fb404dfe77bdddda41abf7a
andreubita <andre.vaz1411@gmail.com>
2020-04-10 04:56:10 +0000

    Private Message command ADDED

--- Kingdoms/src/pt/ubatv/kingdoms/commands/econ/BalanceCommand.java
+++ Kingdoms/src/pt/ubatv/kingdoms/commands/econ/BalanceCommand.java
@@ -17,7 +17,7 @@ public class BalanceCommand implements CommandExecutor {
             Player player = (Player) sender;
             if(args.length == 0){
                 int coins = main.userDataTable.online.get(player.getUniqueId()).getCoins();
-                player.sendMessage("§7Coins §8⮞ §5" + coins + " " + main.textUtils.coinsSymbol);
+                player.sendMessage("§7Coins §8⟿ §5" + coins + " " + main.textUtils.coinsSymbol);
                 return false;
             }
 
@@ -29,7 +29,7 @@ public class BalanceCommand implements CommandExecutor {
                 }
 
                 int coins = main.userDataTable.online.get(target.getUniqueId()).getCoins();
-                player.sendMessage("§5" + target.getName() + "§7's coins §8⯈ §5" + coins + main.textUtils.coinsSymbol);
+                player.sendMessage("§5" + target.getName() + "§7's coins §8⟿ §5" + coins + main.textUtils.coinsSymbol);
                 return false;
             }
 
