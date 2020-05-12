27d2f6ec829b224c11b233505d057d245707afdb
andreubita <andreubita@ubatv.xyz>
2020-05-12 23:51:05 +0100

    test2

--- Hub/src/xyz/ubatv/hub/Main.java
+++ Hub/src/xyz/ubatv/hub/Main.java
@@ -18,8 +18,6 @@ import xyz.ubatv.hub.utils.TextUtils;
 
 public class Main extends JavaPlugin {
 
-    //teste
-
     public static Main instance;
     public TextUtils textUtils;
     public MySQLConnections mySQLConnections;
